05f1844a1ff41be1b5b90c62c7c5a7b054f2f066
Samkaa987 <deneubourg.samuel@gmail.com>
2019-08-13 17:53:10 +0200

    third : add mailer with sendgrid to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+	after_create :welcome_send
 
 
 	has_many :administrations, foreign_key: 'administrator_id', class_name: "Event"
 	has_many :attendances, foreign_key: 'participant_id'
 	has_many :events, through: :attendances
+
+
+
+	def welcome_send
+	   UserMailer.welcome_email(self).deliver_now
+	end
+
 
 end
