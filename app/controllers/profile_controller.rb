dfacb7145f2f375759cb070f0cee107b146c6577
Samkaa987 <deneubourg.samuel@gmail.com>
2019-08-16 14:19:20 +0200

    add edit profile name description

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,33 @@
 class ProfileController < ApplicationController
-		before_action :authenticate_user!, only: [:show]
+		before_action :authenticate_user!, only: [:show, :create, :update]
   def show
   end
+
+  def edit
+
+  	
+  end
+
+  def update
+
+  	current_user.update(profile_params)
+    
+      if current_user.save
+        redirect_to profile_path(current_user.id)
+      else
+        flash.now[:danger] = "Couldn't save."
+        render :edit
+      end
+  	
+  end
+
+
+  def profile_params
+
+	params.require(:user).permit(:first_name, :last_name, :description)
+	  
+  end
+
 end
+
+
